84b3fa620b35f5560ff4bbf686642065538180af
Hadif Hatta <45189724+hdfhtt@users.noreply.github.com>
2023-06-19 22:22:54 +0800

    Create view_recipe.dart

---
+++ lib/view_recipe.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+// TODO: Finalize ViewRecipePage widget.
+class ViewRecipeScreen extends StatelessWidget {
+  const ViewRecipeScreen({super.key});
+
+  // TODO: Include name, description, and image of the recipe.
+  // final List<String> ingredients;
+  // final List<String> instructions;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Add placeholder image.
+    // TODO: List ingredients, and instructions in sequence using map, etc.
+    return(const Text('ViewRecipeScreen()'));
+  }
+}

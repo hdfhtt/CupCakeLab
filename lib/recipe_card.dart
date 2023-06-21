4f21b26929b871d928a09ed86029f9bf0652b674
Nazurin Qamarina Jamaludin <93915306+nazurinjamal@users.noreply.github.com>
2023-06-21 21:28:21 +0800

    Update recipe_card.dart
    Can check the code if betul or not. Saya comment dulu the inital code.

--- lib/recipe_card.dart
+++ lib/recipe_card.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import './view_recipe.dart';
 
 // TODO: Finalize RecipeCard widget.
-class RecipeCard extends StatelessWidget {
+/* class RecipeCard extends StatelessWidget {
   const RecipeCard({super.key, required this.name, required this.description});
 
   final String name;
@@ -31,4 +31,91 @@ class RecipeCard extends StatelessWidget {
     ));
   }
 
+}
+*/
+import 'package:flutter/material.dart';
+void main() {
+  runApp(const CardExamplesApp());
+}
+
+class CardExamplesApp extends StatelessWidget {
+  const CardExamplesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          colorSchemeSeed: const Color(0xff6750a4), useMaterial3: true),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Recipe Card')),
+        body: const Column(
+          children: <Widget>[
+            VanillaCard(),
+            MatchaCard(),
+            Spacer(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class VanillaCard extends StatelessWidget {
+  const VanillaCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        elevation: 0,
+        shape: RoundedRectangleBorder(
+          side: BorderSide(
+            color: Theme.of(context).colorScheme.outline,
+          ),
+          borderRadius: const BorderRadius.all(Radius.circular(12)),
+        ),
+        child: SizedBox(
+          width: 400,
+          height: 300,
+          child: Center(
+            child: 
+              ListTile(
+                title: Image.asset('assets/vanilla.jpeg', fit: BoxFit.fitWidth, height: 200,width: 400 ),
+                subtitle: const Text('\nClassic Vanilla Cupcake\nLorem ipsum dolor sit amet.'),
+              ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MatchaCard extends StatelessWidget {
+  const MatchaCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        elevation: 0,
+        shape: RoundedRectangleBorder(
+          side: BorderSide(
+            color: Theme.of(context).colorScheme.outline,
+          ),
+          borderRadius: const BorderRadius.all(Radius.circular(12)),
+        ),
+        child: SizedBox(
+          width: 400,
+          height: 300,
+          child: Center(
+            child: 
+              ListTile(
+                title: Image.asset('assets/matcha.jpeg', fit: BoxFit.fitWidth, height: 200,width: 400),
+                subtitle: const Text('\nMatcha Green Tea Cupcake\nLorem ipsum dolor sit amet.'),
+              ),
+          ),
+        ),
+      ),
+    );
+  }
 }

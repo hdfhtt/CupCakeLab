d31e596d376b1f59ab3832b695eadd0d51854e15
Hadif Hatta <45189724+hdfhtt@users.noreply.github.com>
2023-06-22 06:00:59 +0800

    Update main.dart
    Added navigation bar functionality

--- lib/main.dart
+++ lib/main.dart
@@ -29,8 +29,24 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final recipes = List<String>.generate(10, (index) => 'Recipe ${index + 1}');
-  
+  Widget _currentFragment = LatestFragment();
+  int _selectedTabIndex = 0;
+
+  void _onDestinationSelected(int index) {
+    setState(() {
+      _selectedTabIndex = index;
+
+      switch(index) {
+        case 1: _currentFragment = const OtherFragment();
+          break;
+        case 2: _currentFragment = const FavoritesFragment();
+          break;
+        default: _currentFragment = LatestFragment();
+          break;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,22 +56,10 @@ class _HomeScreenState extends State<HomeScreen> {
             direction: Axis.vertical,
             children: [
               Expanded(
-                child: ListView.builder(
-                  itemCount: recipes.length,
-                  itemBuilder: (context, index) {
-                    final recipe = recipes[index];
-
-                    if (index == 0) {
-                      /* Create first item as padding */
-                      return const SizedBox(height: 82.0);
-                    } else {
-                      return RecipeCard(
-                        name: recipe,
-                        description: 'Lorem ipsum dolor si amet.'
-                      );
-                    }
-                  },
-                ),
+                child: Container(
+                  alignment: Alignment.center,
+                  child: _currentFragment,
+                )
               ),
             ],
           ),
@@ -83,21 +87,71 @@ class _HomeScreenState extends State<HomeScreen> {
         ],
       ),
       bottomNavigationBar: NavigationBar(
+        labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
+        selectedIndex: _selectedTabIndex,
+        onDestinationSelected: _onDestinationSelected,
         destinations: const <NavigationDestination>[
           NavigationDestination(
-            icon: Icon(Icons.circle_outlined),
+            icon: Icon(Icons.auto_awesome_outlined),
+            selectedIcon: Icon(Icons.auto_awesome),
             label: 'Latest',
           ),
           NavigationDestination(
-            icon: Icon(Icons.circle_outlined),
-            label: 'Other',
+            icon: Icon(Icons.cookie_outlined),
+            selectedIcon: Icon(Icons.cookie),
+            label: 'Other Recipes',
           ),
           NavigationDestination(
-            icon: Icon(Icons.circle_outlined),
+            icon: Icon(Icons.favorite_outline),
+            selectedIcon: Icon(Icons.favorite),
             label: 'Favorites',
           ),
         ]
       )
     );
+  }
+}
+
+class LatestFragment extends StatelessWidget {
+  final recipes = List<String>.generate(10, (index) => 'Recipe ${index + 1}');
+
+  LatestFragment({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: recipes.length,
+      itemBuilder: (context, index) {
+        final recipe = recipes[index];
+
+        if (index == 0) {
+          /* Create first item as padding */
+          return const SizedBox(height: 82.0);
+        } else {
+          return RecipeCard(
+              name: recipe,
+              description: 'Lorem ipsum dolor si amet.'
+          );
+        }
+      },
+    );
+  }
+}
+
+class FavoritesFragment extends StatelessWidget {
+  const FavoritesFragment({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('This will show a list of recipes that has been favorite.');
+  }
+}
+
+class OtherFragment extends StatelessWidget {
+  const OtherFragment({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('This will show any other types of recipe beside cupcakes.');
   }
 }
